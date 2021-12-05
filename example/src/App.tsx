ab84fa680f797575a3106a4f76d08328fb2d0fae
Debdut Karmakar <iawaiponly@gmail.com>
2021-12-06 01:48:26 +0530

    example app

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import YoutubeBg from 'youtube-bg-react'
+import 'youtube-bg-react/dist/index.css'
+
+const App = () => {
+  return <YoutubeBg videoId=""> This is an Youtube Video </YoutubeBg>
+}
+
+export default App
